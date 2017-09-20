5bef30ada0f714981e36a115362cbb3cdb4ba010
Bryann <bryannbarbosa@gmail.com>
2017-09-19 23:05:35 -0300

    excel function

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,8 @@
 const electron = require('electron');
 const ipcRenderer = electron.ipcRenderer;
 
-//document.getElementById('startProcess').addEventListener('click', () => {
-    ipcRenderer.send('test', {name: 'Bryann'});
-//});
+document.getElementById('startProcess').addEventListener('click', () => {
+    ipcRenderer.send('runFile', {start: true});
+});
 
 
